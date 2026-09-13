0b37f8f28d0dc5d648c09f3b8e176a9e07fd4eac
n1sz <dakepera@naver.com>
2026-09-14 01:03:48 +0900

    feat(ios): resume pending telemetry on foreground activation

--- apps/ODIC/ODIC/ODICApp.swift
+++ apps/ODIC/ODIC/ODICApp.swift
@@ -9,9 +9,16 @@ import SwiftUI
 
 @main
 struct ODICApp: App {
+    @Environment(\.scenePhase) private var scenePhase
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .task { await BenchmarkTelemetry.shared.start() }
+                .onChange(of: scenePhase) { _, phase in
+                    if phase == .active {
+                        Task { await BenchmarkTelemetry.shared.start() }
+                    }
+                }
         }
     }
 }
